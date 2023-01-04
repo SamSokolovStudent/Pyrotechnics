cb6023d88c2af256497293153a4e8ebd7614314c
SamSokolovStudent <90578942+SamSokolovStudent@users.noreply.github.com>
2023-01-04 14:31:43 +0100

    Initial commit

---
+++ src/main/java/net/soko/pyrotechnics/PyroTechnicsModClient.java
@@ -0,0 +1,10 @@
+package net.soko.pyrotechnics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PyroTechnicsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
+
